849e5dd97431aaaa5e1b24cd5795354854bc0353
Antonio James <asjames18@gmail.com>
2020-07-06 16:21:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,4 +4,4 @@ def introduction_with_language(name, language)
   puts "Hi , my name is #{name} and I am learning tp program in #{language}."
 end
 
-introduction_with_language "Josh, ruby"
+introduction_with_language "Josh", "ruby"
