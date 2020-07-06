440ea5f39e1efae758fa92c2f549ef33dc3cf0a8
Antonio James <asjames18@gmail.com>
2020-07-06 17:27:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 #lib/introduction.rb
 
-def introduction(name, language)
-  puts "Hi , my name is #{name} and I am learning to program in #{language}."
+def introduction(name)
+  puts "Hi , my name is #{name}."
 end
 
-introduction("Josh", "Ember.js")
+introduction("Josh")
